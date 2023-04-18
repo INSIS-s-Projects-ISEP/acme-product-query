671990257176f4e13fc4a413b74c87f5eed83733
NaironCarneiro <1222620@isep.ipp.pt>
2023-04-18 12:15:02 +0100

    feat: adding eureka server

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import RabbitMQConfig from "./config/rabbitMQConfig";
 
 mongoose.connect('mongodb://mongo/productquery');
 
+
 const app = express();
 
 app.use(express.json());
@@ -14,6 +15,43 @@ app.use(routes);
 
 const rabbitMQConfig = new RabbitMQConfig();
 rabbitMQConfig.connect();
+
+const Eureka = require('eureka-js-client').Eureka;
+
+const os = require('os');
+const instanceId = `${os.hostname()}:${process.env.PORT}`;
+
+
+const client = new Eureka({
+    instance: {
+        app: 'acme-product-query',
+        instanceId,
+        hostName: process.env.HOSTNAME,
+        ipAddr: '127.0.0.1',
+        port: {
+            '$': 3000,
+            '@enabled': 'true'
+        },
+        vipAddress: 'my-service-product-query',
+        dataCenterInfo: {
+            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+            name: 'product-query'
+        }
+    },
+    eureka: {
+        host: 'discovery-system',
+        port: 8761,
+        servicePath: '/eureka/apps/'
+    }
+});
+
+client.start((error: any) => {
+    if (error) {
+        console.log('Error starting Eureka client', error);
+    } else {
+        console.log('Eureka client started successfully');
+    }
+});
 
 app.listen(3000, () =>{
     console.log("Server is Listening");
